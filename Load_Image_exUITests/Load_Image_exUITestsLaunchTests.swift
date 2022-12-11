281a5a9794ca7e4325c325349536622754806fcb
Bullets-jyr <jyr@bullets.co.kr>
2022-12-11 18:09:22 +0900

    Initial Commit

---
+++ Load_Image_exUITests/Load_Image_exUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Load_Image_exUITestsLaunchTests.swift
+//  Load_Image_exUITests
+//
+//  Created by bullets on 2022/12/11.
+//
+
+import XCTest
+
+final class Load_Image_exUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
